ea63680dd17db383a396b9a1bb459efe71646adc
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-10-19 20:30:28 -0500

    initial index.js

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+## Description
+
+I made this for a challenge for the UTA Fullstack Development bootcamp. It's a simple quiz that reacts to a users button clicks to start and move through the quiz.
+It also keeps track of how long the user takes to complete the quiz, and ends the quiz when time runs out. An incorrect answer also reduced the remaining time by 10 seconds.
+After the quiz is over the user can input their initials to save their score onto the local storage, which can be cleared from the highscores page.
+<br>
+This challenge helped me further my skills with getting and using a user's input to affect a webpage, and helped me learn how to store, retrieve and clear data from the local storage.
+
+## Usage
+
+Link to the webpage: https://devonmcfarlen.github.io/UTA-Bootcamp-Module4-challenge-JSQuiz/
+
+![alt text](./assets/images/websiteSS.png)
+
+
+## License
+
+MIT License
--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const gM = require('generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {type: 'input', message: 'Please enter a title for the README.', name: 'title'},
+    {type: 'input', message: 'Please write a short description for your project', name: 'desc'},
+    {type: 'input', message: 'How do you install your project?', name: 'install'},
+    {type: 'input', message: 'How do you use your project?', name: 'usage'},
+    {type: 'input', message: 'What are the contribution guidelines for your project?', name: 'contri'},
+    {type: 'input', message: 'How should users test your project?', name: test},
+    {type: 'list', message: 'What License would you like to use for your project?', name: 'license'},
+    {type: 'input', message: 'Whats your GitHub username?', name: 'gitHub'},
+    {type: 'input', message: 'Whats your email address?', name: 'email'}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.html`, data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(writeToFile(response.title, gM.generateMarkdown(response)));
+}
 
 // Function call to initialize app
 init();
